7281f194d4dbfa7f368d13633ed6c0a525e1bdc9
--global <--global>
2024-09-08 22:55:39 +0200

    correc

--- png-to-data-uri.py
+++ png-to-data-uri.py
@@ -5,7 +5,7 @@ import f_retIMG
 
 # png_uri=DataURI.from_file('Axelfall-MR240430-CorT20000.png')
 # print(str(png_uri))
-f_logging.f_initLog()
+f_initLog()
 
 def f_FOLDERdotSerialAppend(in_folder,in_appendfile):
     folder_img_list=f_retIMG.f_allimg_thisfolder(in_folder)
