55cb2ffa26d02aa0b9f2349c7598375496838cf8
--global <--global>
2024-09-09 00:13:38 +0200

    no images online

---
+++ f_imgfold_datauri_js.py
@@ -0,0 +1,30 @@
+from datauri import DataURI
+from f_logging import f_initLog
+from f_logging import f_log as f_l
+import f_retIMG
+
+# png_uri=DataURI.from_file('Axelfall-MR240430-CorT20000.png')
+# print(str(png_uri))
+
+f_initLog()
+
+# def f_FOLDERdotSerialAppend(in_folder,in_appendfile):
+#     folder_img_list=f_retIMG.f_allimg_thisfolder(in_folder)
+#     f_l(folder_img_list)
+#     f=open(in_appendfile,'a')
+#     f.write('const '+in_folder+' = {')
+#     for i,this_image in enumerate(folder_img_list):
+#         f_l(str(i)+' : '+this_image)
+#         png_uri=DataURI.from_file(this_image)
+#         f.write('image'+str(i)+': \''+str(png_uri)+'\', \n')
+#         f_l('image'+str(i)+': \'-datauriremoved-\', ')
+#     f.write('}')
+#     f.close()
+
+def f_imgFl_PathNnames():
+    f_out=listdir(thispath)
+    f_out.sort()
+    ex_dirs, ex_files= f_retIMG.f_retDirsFiles(f_out)
+    f_l(ex_dirs)
+
+f_FOLDERdotSerialAppend('CorT2','dumpit.txt')
